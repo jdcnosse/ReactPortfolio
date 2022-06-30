bb6afc2ef878b18b2438401e237a3150d6dfc671
James <jdcnosse@gmail.com>
2022-06-29 17:15:06 -0700

    Creating Pages & Adding Router to Header/Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import RequestPage from './pages/RequestPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <br />
+      <br />
+      <br />
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='about' element={<AboutPage />} />
+        <Route path='directory' element={<ContactPage />} />
+        <Route path='request' element={<RequestPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { Navbar, NavbarToggler, Nav, NavItem, Collapse } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <Navbar dark color='dark' expand='md' fixed='top'>
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)}/>
+            <Collapse isOpen={menuOpen} navbar>
+                <Nav className='mx-auto' navbar>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/'>
+                            <i className='fa fa-home fa-lg' /> Home
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/about'>
+                            <i className='fa fa-info fa-lg' /> About Us
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/contact'>
+                            <i className='fa fa-address-card fa-lg' /> Contact Us
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/request'>
+                            <i className='fa fa-comment fa-lg' /> Request an Issue
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
+    );
+};
+
+export default Header;
